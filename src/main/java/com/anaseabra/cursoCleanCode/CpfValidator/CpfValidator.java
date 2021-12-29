53ee9e3cef94a90bd3bc98b9204316340e367929
anabarbaraseabra <anabarbaraseabra@gmail.com>
2021-12-28 23:10:53 -0300

    Refactoring CPF validator

--- src/main/java/com/anaseabra/cursoCleanCode/CpfValidator/CpfValidator.java
+++ src/main/java/com/anaseabra/cursoCleanCode/CpfValidator/CpfValidator.java
@@ -9,34 +9,43 @@ import java.util.stream.Collectors;
 @Component
 public class CpfValidator {
 
-    public boolean validateCpf(String cpf) {
+    private final int FACTOR_10 = 10;
+    private final int FACTOR_11 = 11;
+
+    //step-down rule -> leitura de cima para baixo, de acordo com a ordem que os metodos forem aparecendo (na medida do possível)
+
+    public boolean validateCpf(String rawCpf) {
+        if (rawCpf == null) {
+         return false;
+        }
+
+        String cpf = formatCPFString(rawCpf);
+
         if (!isStringCPFValid(cpf) || hasEqualsCharacters(cpf)) {
             return false;
         }
-        cpf = formatCPFString(cpf);
 
-        int tenDigit = calculateDigit(cpf, 10);
-        int elevenDigit = calculateDigit(cpf, 11);
+        int tenDigit = calculateDigit(cpf, FACTOR_10);
+        int elevenDigit = calculateDigit(cpf, FACTOR_11);
 
         String finalDigits = String.format("%d%d", tenDigit, elevenDigit);
         return finalDigits.equals(cpf.substring(9, 11));
     }
 
-    private boolean hasEqualsCharacters(String cpf) {
-        List<String> charArray = Arrays.stream(cpf.split("")).collect(Collectors.toList());
-        return charArray.stream().allMatch(chr -> chr.equals(charArray.get(0)));
+    private String formatCPFString(String cpf) {
+        return cpf.replaceAll("\\D", "");
     }
 
     private boolean isStringCPFValid(String cpf) {
-        if (cpf == null) {
+        if (cpf == null ) {
             return false;
         }
-        String formatCpf = formatCPFString(cpf);
-        return formatCpf.length() == 11;
+        return cpf.length() == 11;
     }
 
-    private String formatCPFString(String cpf) {
-        return cpf.replaceAll("\\D", "");
+    private boolean hasEqualsCharacters(String cpf) {
+        List<String> charArray = Arrays.stream(cpf.split("")).collect(Collectors.toList());
+        return charArray.stream().allMatch(chr -> chr.equals(charArray.get(0)));
     }
 
     private Integer calculateDigit(String cpf, int factor) {
